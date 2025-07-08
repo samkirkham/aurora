4d9537cdac7968a9740a925888418ce9d4c567ee
Sam Kirkham <s.d.kirkham@gmail.com>
2025-07-08 10:36:40 +0100

    Script to convert tongue_model.csv into lookup dict Pickle file

---
+++ Resonance/build_model.py
@@ -0,0 +1,17 @@
+"""
+Read tongue_model.csv data, create a lookup dictionary for faster processing, save to Pickle file
+"""
+
+import polars as pl
+import pickle
+
+tongue_shapes = pl.read_csv("Resonance/tongue_model.csv")
+
+tongue_lookup = {}
+for (f1, f2), group in tongue_shapes.group_by(["f1_num", "f2_num"]):
+    points = group.select(["X.transposed", "Y.transposed"]).to_numpy()
+    tongue_lookup[(f1, f2)] = points
+
+# save as pickle
+with open("tongue_model.pkl", "wb") as f:
+    pickle.dump(tongue_lookup, f)
