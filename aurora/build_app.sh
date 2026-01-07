fcd7e5eefb6a98b71d586a964e6164dbe309b02d
Sam Kirkham <s.d.kirkham@gmail.com>
2026-01-07 16:17:42 +0000

    Removed comments

--- aurora/build_app.sh
+++ aurora/build_app.sh
@@ -1,6 +1,1 @@
-# NOTES
-# - this works in the sense that it builds the app
-# - but the app doesn't seem to take any audio input!
-# - not a priority for now, just focus on getting the actual python version working properly
-
 pyinstaller --onefile --windowed --add-binary '/opt/homebrew/Cellar/portaudio/19.7.0/lib/libportaudio.2.dylib:.' aurora/aurora.py
